a0047784c24beb9f9e9bacfdaf6abf3ff04ace37
Semir Iljazi <iljazi.semir22@gmail.com>
2022-07-24 20:32:34 +0200

    Fixed gitignore

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    reactStrictMode: true,
+    env: {
+    // NEXT_APP_API_URL: "http://localhost:7800",
+    NEXT_APP_API_URL: "https://compoz-api.herokuapp.com",
+    },
+    };
